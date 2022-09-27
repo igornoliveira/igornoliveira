c61eb03db9362851a79b4563129f891d57aa3313
Igor Nascimento <igrnascolive@gmail.com>
2022-09-27 16:18:25 -0300

    Adicionando novo post

--- deploy.sh
+++ deploy.sh
@@ -9,25 +9,20 @@ echo "Realizando novo commit no diretorio ${PWD}"
 echo "##########################################"
 git add .
 git commit -m "${commit}"
-sleep 2
 git push origin main
 
-sleep 2
 cd ..
 echo "##########################################"
 echo "Realizando novo commit no diretorio ${PWD}"
 echo "##########################################"
 git add .
 git commit -m "${commit}"
-sleep 2
 git push origin main
 
-sleep 2
 cd content
 echo "##########################################"
 echo "Realizando novo commit no diretorio ${PWD}"
 echo "##########################################"
 git add .
 git commit -m "${commit}"
-sleep 2
 git push origin master
