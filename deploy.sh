db230733352cab31e3c4462b18bc9199882c0d4c
Igor Nascimento <igrnascolive@gmail.com>
2022-09-27 16:00:07 -0300

    Adicionando

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,21 @@ commit="no message for this commit"
 [[ "${1}" ]] && commit=${1}
 
 cd public
+pwd
 git add .
 git commit -m "${commit}"
 git push origin main
 
+sleep 5
 cd ..
+pwd
 git add .
 git commit -m "${commit}"
 git push origin main
 
+sleep 5
 cd content
+pwd
 git add .
 git commit -m "${commit}"
-git push origin main
+git push origin master
